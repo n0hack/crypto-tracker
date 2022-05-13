5a6d6b9e05f481e0cf5c7c2931532b4b2d890b07
n0hack <nohack-@naver.com>
2022-05-13 21:21:42 +0900

    fix: Chart 색상 추가

---
+++ src/context/theme.ts
@@ -0,0 +1,25 @@
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+  bgColor: '#fff',
+  textColor: '#000',
+  accentColor: '#000',
+  buttonLabelColor: '#000',
+  buttonBorderColor: '#000',
+  spinnerColor: '',
+  listBgColor: '',
+  listTextColor: '#000',
+  chartColor: 'light',
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: '#2f3640',
+  textColor: '#fff',
+  accentColor: '#fff',
+  buttonLabelColor: '#fff',
+  buttonBorderColor: '#fff',
+  spinnerColor: '#fff',
+  listBgColor: '#fff',
+  listTextColor: '#000',
+  chartColor: 'dark',
+};
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,5 +1,15 @@
 import 'styled-components';
 
 declare module 'styled-components' {
-  export interface DefaultTheme {}
+  export interface DefaultTheme {
+    bgColor: string;
+    textColor: string;
+    accentColor: string;
+    buttonLabelColor: string;
+    buttonBorderColor: string;
+    spinnerColor: string;
+    listBgColor: string;
+    listTextColor: string;
+    chartColor: 'light' | 'dark';
+  }
 }
